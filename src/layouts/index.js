9408e1936ad07123e453565c8ed6ef066d4d9b81
pavel <nikitin.pavel.nik@yandex.ru>
2017-12-16 18:15:11 +0700

    second commit

---
+++ src/layouts/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+import './../scss/index.scss'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Fotogallery"
+      meta={[
+        { name: 'description', content: 'BLACKTIE' },
+        { name: 'keywords', content: 'black, ties' },
+      ]}
+    />
+    <div>
+      <div className="menu">
+        <div className="logo">Фотогалерея</div>
+        <div className="menu-list hidden-xs">
+        </div>
+        <div className="clear"></div>
+      </div>
+      {children()}
+      <div className="footer">
+        <div className="container">
+          <div className="row centered">
+            <h2>You Can Contact Us</h2>
+            <h5>HELLO@LANDINGSUMO.COM</h5>
+
+            <p className="mt">
+              <a href="#"><i className="ion-social-twitter"></i></a>
+              <a href="#"><i className="ion-social-dribbble"></i></a>
+              <a href="#"><i className="ion-social-instagram"></i></a>
+              <a href="#"><i className="ion-social-facebook"></i></a>
+              <a href="#"><i className="ion-social-pinterest"></i></a>
+              <a href="#"><i className="ion-social-tumblr"></i></a>
+            </p>
+            <h6 className="mt">COPYRIGHT 2014 - LANDING SUMO</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/index.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import appStoreImg from '../img/app-store.png'
+import googlePlayImg from '../img/google-play.png'
+import phoneImg from '../img/phone.png'
+import GalleryImage from '../components/GalleryImage'
+import NavLeft from '../components/NavLeft'
+import NavRight from '../components/NavRight'
+import GalleryModal from '../components/GalleryModal'
+
+
+
+let imgUrls = [
+  'http://mypresentation.ru/documents/884219c23e6888b602805f0674eeb70e/img37.jpg',
+  'https://econet.ru/uploads/pictures/119697/content_10__econet_ru.jpg',
+  'http://www.pavelin.ru/images/stories/leopard/leo_011.jpg',
+  'http://acrosscars.com/uploads/fotos/2015-fnr_concept6.jpg',
+  'https://img.day.az/clickable/00/0/354223_004.jpg',
+  'https://4589861635b91edaa841-e4ddf20bbb131c4268e6018b2e3d1bb8.ssl.cf1.rackcdn.com/426850.jpg',
+  'http://ij.drivenn.ru/jvrcgg1hl076q_1o0xzqe.jpeg',
+  'http://luxfon.com/pic/201208/800x600/luxfon.com-16378.jpg',
+  'http://luxfon.com/pic/201211/800x600/luxfon.com-17656.jpg',
+  'https://www.motto.net.ua/pic/201209/800x600/motto.net.ua-23658.jpg',
+  'http://img.desktopwallpapers.ru/women/pics/6e78334bca1eb1f89ab15.jpg',
+  'https://www.motto.net.ua/pic/201210/800x600/motto.net.ua-43652.jpg'
+];
+
+
+
+class IndexPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      url: '',
+      fullScreenPhotoIndex: 0
+    }
+
+    this.openModal = this.openModal.bind(this);
+
+    this.closeModal = this.closeModal.bind(this);
+    this.handlePhotoNavClick = this.handlePhotoNavClick.bind(this);
+      
+  }
+
+   // Открытие модального окна
+   openModal(url,e, index) {
+    
+    e.preventDefault();
+
+    this.setState({ showModal: true, url: url, fullScreenPhotoIndex: index})
+  };
+
+  // Закрытие модального окна
+  closeModal() {
+    this.setState({showModal: false, url: ''})
+  };
+
+
+  // Навигация по фотографиям
+  handlePhotoNavClick(e,delta) {
+
+    e.preventDefault();
+
+    var Index = this.state.fullScreenPhotoIndex + delta;
+
+    // Циклирование просмотра, возврат к первому фото после просмотра последнего и наоборот
+    if(Index < 0) {
+      Index = imgUrls.length - 1;
+    } else if (Index >= imgUrls.length) {
+      Index = 0;
+    }
+
+    this.setState({fullScreenPhotoIndex: Index});
+
+  };
+
+
+  render() {
+    var numImages = imgUrls.length;
+    var fullPhotoURL = this.state.url ;
+    var fullPhotoImageIndex = this.state.fullScreenPhotoIndex;
+    if (fullPhotoImageIndex >= 0 && fullPhotoImageIndex < numImages) {
+      fullPhotoURL = imgUrls[fullPhotoImageIndex];
+    }
+
+    return (
+      <div className='container-fluid gallery-container'>
+        <div className='row'>
+          {imgUrls.map((url, Index) => {
+            
+            return <div key={Index} className='col-xs-6 col-md-3 col-lg-2'>
+              <div className='gallery-card'>
+                <GalleryImage src={url}  alt={'Image number ' + (Index+ 1)}/>
+
+                <i
+                  className='icon ion-search'
+                  value={url}
+                  onClick={(e) => this.openModal(url,e, Index)}></i>
+              </div>
+            </div>
+          })
+}   
+        </div>
+        <GalleryModal isOpen={this.state.showModal}  onClick={this.closeModal} src={fullPhotoURL}>
+            <NavLeft onClick={(e,delta) => this.handlePhotoNavClick(e, -1)}></NavLeft>
+            <NavRight onClick={(e,delta) => this.handlePhotoNavClick(e, 1)}></NavRight>
+        </GalleryModal> 
+      </div>
+      
+    )
+  }
+
+}
+
+export default IndexPage
